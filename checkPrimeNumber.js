862bea33440242cd6ac76f1617a16fbcdbdb85dd
Priya Kumari <priyakumariyt05@gmail.com>
2024-02-17 13:42:15 +0530

    update

--- checkPrimeNumber.js
+++ checkPrimeNumber.js
@@ -1,33 +1,66 @@
+var a = 2;
+var b = 5;
+
+var result = "";
+for (i = a; i <= b; i++) {
+  var isPrime = 1;
+  for (j = 2; j < i; j++) {
+    if (i % j == 0) {
+      isPrime++;
+    }
+  }
+  if (i > 1 && isPrime == 1) {
+    result = result + i + " ";
+  }
+}
+console.log(result);
+// CHECK PRIME NUMBER 1 T0 100
+function primeNo_checked(num) {
+  let count = 0;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      count++;
+    }
+  }
+  if (count == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+var n = 100;
+var arr = [];
+for (var i = 0; i <= n; i++) {
+  if (primeNo_checked(i)) {
+    arr.push(i);
+  }
+}
+// console.log(arr);
+
+// CHECKED PRIME IN AN ARRAY
+var arr = [3,5,7,11,13,19,23,29,17,12,15,14,27]
+function primeNo_checked(num){
+let count = 0;
+for(let i = 2; i<num; i++){
+        if(num%i==0){
+                count++
+        }
+}
+if(count==0){
+        return true
+}
+else{
+        return false
+}
+
+}
 
 let countPrime_no = 0;
 for(let i = 0; i<arr.length; i++){
-    let res = primeNo_checked(arr[i])
-    if(res==true){
-            countPrime_no++
-    }
+        let res = primeNo_checked(arr[i])
+        if(res==true){
+                countPrime_no++
+        }
 }
 console.log(countPrime_no)
-
-// CHECK PRIME NUMBER 1 T0 100
-function primeNo_checked(num) {
-    let count = 0;
-  
-    for (let i = 2; i < num; i++) {
-      if (num % i == 0) {
-        count++;
-      }
-    }
-    if (count == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  var n = 100;
-  var arr = [];
-  for (var i = 0; i <= n; i++) {
-    if (primeNo_checked(i)) {
-      arr.push(i);
-    }
-  }
-  console.log(arr);
--- reverseString.js
+++ reverseString.js
@@ -1,10 +1,4 @@
-var num = 971;
-var str = num.toString();
-var reverse = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  reverse += str[i];
-}
-console.log("reverse",reverse);
+
 
 // 2nd question
 
@@ -15,3 +9,15 @@ for (let i = str.length - 1; i >= 0; i--) {
   reverse += str[i];
 }
 console.log("reverse",reverse);
+// 3rd example
+
+let str1 = " Priya Kumari"
+
+const isReverse = (str)=>{
+    let rev = ""
+for(let i = str.length-1; i>=0; i--){
+  rev += str[i]
+}
+return rev
+ }
+console.log(isReverse(str1))
