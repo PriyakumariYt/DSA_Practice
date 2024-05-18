8117d6b5dccbd788c327626e30c3854bb3bcec16
Priya Kumari <priyakumariyt05@gmail.com>
2024-05-18 17:31:41 +0530

    upload Dsa

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -8,3 +8,17 @@ if (rev === str) {
   ans = "yes";
 }
 console.log(ans);
+
+var isPalindrome = function(x) {
+  var str = x.toString();
+ var rev = "";
+ 
+for (let i = str.length - 1; i >= 0; i--) {
+     rev += str[i];
+ }
+ 
+ return rev === str;
+};
+
+let x = 121
+console.log(isPalindrome(x))
